a427315b8ddab7c52697c3130238ca09ce89cbeb
Lonewolf <lonewolf-320@users.noreply.github.com>
2025-12-29 15:30:08 +0500

    Initial deploy

---
+++ src/pages/Adoption.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PetListings from '../components/PetListings';
+
+const Adoption = () => {
+    return (
+        <div className="pt-20">
+            <PetListings />
+        </div>
+    );
+};
+
+export default Adoption;
